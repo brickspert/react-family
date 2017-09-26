291064f83da7861e88a9e1e6bf71697113407a62
fanjl <576679268@qq.com>
2017-09-26 20:38:47 +0800

    configuring HMR with Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,4 +5,11 @@ import promiseMiddleware from './middleware/promiseMiddleware'
 
 let store = createStore(combineReducers, applyMiddleware(promiseMiddleware));
 
+if (module.hot) {
+    module.hot.accept("./reducers", () => {
+        const nextReducer = require("./reducers").default;
+        store.replaceReducer(nextReducer);
+    });
+}
+
 export default store;
